5e6f6e865efcf8933db0d8704a856f714e6708ea
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-07-22 15:08:56 +0500

    reacttrouter

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+function Navbar() {
+   
+  return (
+    <div>
+        <nav>
+            <ul>
+              <a href='/'      ><li>Home</li></a>
+              <a href='/About' ><li>About</li></a>
+              <a href='/Login' ><li>Login</li></a>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
